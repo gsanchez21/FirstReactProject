d8f18c1c3d6c5a526cc1bd5d1977eaf6921639c5
gsanchez21 <medina21g@yahoo.com>
2023-07-26 22:46:11 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
+import {puppyList} from './data'
+
+console.log(puppyList);
 
 function App() {
-  //const [count, setCount] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList);
+console.log(puppies)
   return (
-    <div></div>
-  )
+    <div className='App'>
+      {  puppies.map((puppy) => {
+      return <p>{puppy.name}</p>
+    })}
+    </div>
+  );
 }
 //     <>
 //       <div>
