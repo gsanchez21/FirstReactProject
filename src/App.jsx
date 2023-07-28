2c0570e6067b100c683c8cf848a097de5984f58d
gsanchez21 <medina21g@yahoo.com>
2023-07-27 20:38:00 -0400

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+import { useState } from "react";
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
-import {puppyList} from './data'
+import { puppyList } from "./data";
 
 console.log(puppyList);
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup)
+
   return (
-    <div className='App'>
-      {  puppies.map((puppy) => {
-      return <p>{puppy.name}</p>
-    })}
+    <div className="App">
+
+      { featPupId && (<div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age} </li>
+          <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>)}
+      
+      {puppies.map((puppy) => {
+          return (
+          <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}
+          </p> ); 
+      })} 
     </div>
   );
 }
@@ -41,5 +55,5 @@ console.log(puppies)
 //     </>
 //   )
 // }
-  
-export default App
+
+export default App;
